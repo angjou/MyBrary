b20517a5d9ce7594164afd2ff544b9869ec07e36
angjou <andeladmv@gmail.com>
2020-01-25 22:55:54 +0100

    popravljen nearby

---
+++ app/src/main/java/com/example/mybrary/ui/nearby/MyAdapter.java
@@ -0,0 +1,61 @@
+package com.example.mybrary.ui.nearby;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mybrary.R;
+
+public class MyAdapter  extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+
+        CardView cardView;
+        TextView name;
+        public MyViewHolder(View v) {
+            super(v);
+            cardView =  v.findViewById(R.id.cardview);
+            name =  v.findViewById(R.id.nearby_name);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                                     int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_item, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.name.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
--- app/src/main/java/com/example/mybrary/ui/nearby/NearbyFragment.java
+++ app/src/main/java/com/example/mybrary/ui/nearby/NearbyFragment.java
@@ -11,25 +11,32 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mybrary.R;
 
 public class NearbyFragment extends Fragment {
 
     private NearbyViewModel nearbyViewModel;
-
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    String[] myDataset = {"name"};
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         nearbyViewModel =
                 ViewModelProviders.of(this).get(NearbyViewModel.class);
         View root = inflater.inflate(R.layout.fragment_nearby, container, false);
-        final TextView textView = root.findViewById(R.id.text_nearby);
-        nearbyViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        recyclerView = root.findViewById(R.id.recycler_view);
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this.getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
+
+
         return root;
     }
 }
